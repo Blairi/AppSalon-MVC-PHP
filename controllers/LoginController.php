ba6c7df5732e39c66efabe686a60ce16a244db3c
Blairi <axel.avi.fer@gmail.com>
2022-03-06 23:05:18 -0600

    Iniciando Repositorio

---
+++ controllers/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Controllers;
+
+class LoginController{
+    public static function login(){
+        echo "Desde login xd";
+    }
+
+
+    public static function logout(){
+        echo "Desde logout xd";
+    }
+
+
+    public static function olvide(){
+        echo "Desde olvide xd";
+    }
+
+    
+    public static function recuperar(){
+        echo "Desde recuperar xd";
+    }
+
+
+    public static function crear(){
+        echo "Desde crear xd";
+    }
+}
