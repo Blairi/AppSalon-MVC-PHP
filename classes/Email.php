008e9c88cf8f9b70c2f95fdc49678b49797aae14
Blairi <axel.avi.fer@gmail.com>
2022-03-09 22:51:33 -0600

    Restablecer password terminado

--- classes/Email.php
+++ classes/Email.php
@@ -48,4 +48,38 @@ class Email{
         //Enviar el email
         $mail->send();
     }
+
+
+    public function enviarInstrucciones(){
+
+        // Crear el objeto de email
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Port = 2525;
+        $mail->Username = '527672c9ae1331';
+        $mail->Password = 'e85b4e1fdd44ed';
+
+        $mail->setFrom("cuentas@appsalon.com");
+        $mail->addAddress("cuentas@appsalon.com", "AppSalon.com");
+        $mail->Subject = "Restablece tu password";
+
+        // Set HTML
+        $mail->isHTML(TRUE);
+        $mail->CharSet = "UTF-8";
+
+        $contenido = '<html>';
+        $contenido .= '<p><strong>Hola ' . $this->nombre .'</strong> Has solicitado restablecer tu password, sigue el siguiente enlace para hacerlo.</p>';
+        $contenido .= '<p>Presiona aquí: <a href="http://localhost/recuperar?token='. $this->token .'">Restablecer Password</a></p>';
+        $contenido .= "<p>Si tú no solicitaste este cambio, puedes ignorar el mensaje.</p>";
+        $contenido .= '</html>';
+
+        $mail->Body = $contenido;
+        
+
+        //Enviar el email
+        $mail->send();
+    }
+
 }
--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -62,12 +62,85 @@ class LoginController{
 
 
     public static function olvide(Router $router){
-        $router->render("auth/olvide-password", []);
+
+        $alertas = [];
+
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            $auth = new Usuario($_POST);
+            $alertas = $auth->validarEmail();
+
+            if(empty($alertas)){
+                $usuario = Usuario::where("email", $auth->email);
+                if($usuario && $usuario->confirmado === "1"){
+                    // Generar un token
+                    $usuario->crearToken();
+                    $usuario->guardar();
+
+                    // Enviar el email
+                    $email = new Email($usuario->email, $usuario->nombre, $usuario->token);
+                    $email->enviarInstrucciones();
+
+
+                    // Alerta de exito
+                    Usuario::setAlerta("exito", "Revisa tu email");
+                }
+                else{
+                    Usuario::setAlerta("error", "El usuario no existe o no está confirmado");
+                }
+            }
+        }
+
+        $alertas = Usuario::getAlertas();
+
+        $router->render("auth/olvide-password", [
+            "alertas" => $alertas
+        ]);
     }
 
     
-    public static function recuperar(){
-        echo "Desde recuperar xd";
+    public static function recuperar(Router $router){
+
+        $alertas = [];
+        $error = false;
+
+        $token = s($_GET["token"]);
+
+        // Buscar usuario por su token
+        $usuario = Usuario::where("token", $token);
+        
+        if(empty($usuario)){
+            Usuario::setAlerta("error", "Token No Valido");
+            $error = true;
+        }
+        
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            // Leer el nuevo password y guardar
+
+            $password = new Usuario($_POST);
+            $alertas = $password->valdiarPassword();
+
+            if(empty($alertas)){
+                $usuario->password = null;
+
+                $usuario->password = $password->password;
+                $usuario->hashPassword();
+
+                $usuario->token = null;
+
+                $resultado = $usuario->guardar();
+
+                if($resultado){
+                    header("Location: /");
+                }
+            }
+        }
+
+        $alertas = Usuario::getAlertas();
+
+        $router->render("auth/recuperar-password",[
+            "alertas" => $alertas,
+            "error" => $error
+        ]);
     }
 
 
