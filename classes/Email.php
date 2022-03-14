0754082baeb8d85a524a296b6005ddc31fd0cff4
Blairi <axel.avi.fer@gmail.com>
2022-03-14 08:35:18 -0600

    Hotfix mail

--- classes/Email.php
+++ classes/Email.php
@@ -38,7 +38,9 @@ class Email{
 
         $contenido = '<html>';
         $contenido .= '<p><strong>Hola ' . $this->nombre .'</strong> Has creado tu cuenta en App Salon, solo debes confirmarla presionando el siguiente enlace</p>';
-        $contenido .= '<p>Presiona aquí: <a href="http://localhost/confirmar-cuenta?token='. $this->token .'">Confirmar Cuenta</a></p>';
+        $contenido .= '<p>Presiona aquí: <a href="';
+        $contenido .= $_ENV["DB_HOST"];
+        $contenido .= '/confirmar-cuenta?token='. $this->token .'">Confirmar Cuenta</a></p>';
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje.</p>";
         $contenido .= '</html>';
 
